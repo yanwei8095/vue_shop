75ebc524e72658c2b3e87f23f8295099c81cd8b1
Yan wei <yanwei2007095@qq.com>
2019-04-24 19:16:31 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+new Vue({
+  el: '#app',
+  /* components: { App },
+  template: '<App/>' */
+  render:h=>h(App),
+  /*render: function (createElement) {
+    return createElement(App)  // 返回<App/>
+  }*/
+  router,
+})
